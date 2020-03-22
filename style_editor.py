fcfdd5c78f09070180cf8a5df8d1cf08ef33168b
Boris Blokland <borisblokland@gmail.com>
2020-03-22 22:42:52 +0100

    Got lyric file interval replacement working

--- style_editor.py
+++ style_editor.py
@@ -43,6 +43,11 @@ def update_lyric_file(folder, filename):
         exit(-1)
     print(f"Updating lyric file: {filename}")
 
+    pattern_replace_tuples = []
+    pattern_replace_tuples.append("nLineInterval=\"[^\"]*\" nInner", "nLineInterval=\"160\" nInner")
+
+    convert_file(f"{folder}/{filename}", pattern_replace_tuples)
+
 def update_project_file(folder, filename):
     if not os.path.exists(f"{folder}/{filename}"):
         print(f"file doesn't exists! {folder}/{filename}")
@@ -53,36 +58,54 @@ def update_file(lyric_file_folder, project_file_folder, filename):
     update_lyric_file(lyric_file_folder,f"{filename}.rzlrc")
     update_project_file(project_file_folder,f"{filename}.rzmmpj")
 
-def find_float(text):
-    result = re.search('[0-9]+\.?[0-9]*', f"{text}")
-    if result:
-        original_value = float(result.group(0))
-        return original_value
+def convert_file_to_utf16(filename):
+    print("Converting file from utf-8 to utf-16")
+    content = ''
+    try:
+        with open(filename, 'r', encoding="utf-8") as f:
+            content = f.read()
+        with open(filename, 'w', encoding="utf-16") as f:
+            f.write(content)
+    except UnicodeDecodeError:
+        print("File is probably already utf-16")
 
-def add_to_attribute_value(line, attribute, add):
-    result = re.search(f'{attribute}="[-+]?[0-9]*\.?[0-9]*"', line)
-    if (result):
-        matched_result = result.group(0) # matched something like dEndTime="34.34"
-        old_value = find_float(matched_result)
-        new_value = old_value + add
-        line = re.sub(f'{attribute}="{old_value}"', f'{attribute}="{new_value}"', line)
-    return line
+def convert_file(filename, pattern_replace_tuples):
 
-def replace_times_regex(lyricfile, delay):
+    convert_file_to_utf16(filename)
+
     linelist = []
-    with open(lyricfile, 'r', encoding='utf-16-le') as f:
-        print("Reading txt file")
+    with open(filename, 'r', encoding='utf-16-le') as f:
         linelist = f.readlines()
+
+    for pattern_replace_tuple in pattern_replace_tuples:
+        print(f"pattern: {pattern_replace_tuple[0]}")
+        print(f"replacement: {pattern_replace_tuple[1]}")
     
     for i in range(len(linelist)):
-        linelist[i] = add_to_attribute_value(linelist[i], 'dStartTime', delay)
-        linelist[i] = add_to_attribute_value(linelist[i], 'dEndTime', delay)
+        for pattern_replace_tuple in pattern_replace_tuples:
+            pattern = pattern_replace_tuple[0]
+            replacement = pattern_replace_tuple[1]
+            result = re.search(pattern, linelist[i])
+            if result:
+                print(f"found a match at line {i}: {result}")
+                linelist[i] = re.sub(pattern, replacement, linelist[i])
 
     print("lines read", len(linelist))
 
     print("write changes back to file")
-    with open(lyricfile, 'w', encoding='utf-16-le') as f:
+    with open(filename, 'w', encoding='utf-16-le') as f:
         f.writelines(linelist)
 
+
+def sandbox():
+    test_string = "<para nStartLine=\"1\" nAlign=\"0\" nAngle=\"0\" nLineInterval=\"80\" nInnerAlign=\"1\" nScrollDirection=\"0\" nGradient=\"5\" dAppears=\"0.5\"/>"
+    result = re.search("nLineInterval=\"[^\"]*\" nInner", test_string)
+    print(result)
+
+
 if __name__ == '__main__':
-    edit_style()
+    DEBUG = False
+    if DEBUG:
+        sandbox()
+    else:
+        edit_style()
